48b3de156bb80702736daf762f8405651685db62
sovitranjitkar <sovitranjitkar@gmail.com>
2022-05-11 10:10:04 +0545

    login api part 1/2

---
+++ controllers/auth.js
@@ -0,0 +1,12 @@
+const User = require("../models/user");
+
+exports.userLogin = async (req, res) => {
+  // username check 
+  // password check
+  // if login success return token
+  // if login failed return exception.
+  
+  return res.json({
+    msg: 'Login successfully..'
+  });
+};

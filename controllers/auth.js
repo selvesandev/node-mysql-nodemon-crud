f208074dee1ffee3872f1c070889ed74635913bd
sovitranjitkar <sovitranjitkar@gmail.com>
2022-05-16 08:53:08 +0545

    user api updated validation and select

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,23 +3,24 @@ var jwt = require('jsonwebtoken');
 
 exports.userLogin = async (req, res) => {
 
-  const { username, password } = req.body;
+  const { username = '', password = '' } = req.body;
+  if (username.length === 0 || password.length === 0) return res.status(403).send({ msg: 'Username and Password is a required field' });
 
-  User.readSingle(username, function (err, user) {
+  User.readSingleByEmail(username, function (err, user) {
     if (err) {
       return res.status(403).send(err);
     }
 
     if (!user) return res.sstatus(403).send({ msg: 'User not found' });
 
-    if (user.password !== password) return res.status(403).send({ msg: 'Password is incorrect' });
+    if (user.auth_password !== password) return res.status(403).send({ msg: 'Password is incorrect' });
 
     res.json({
       msg: 'Logged in',
       token: jwt.sign({
         id: user.id,
         email: user.email,
-      },'salt_secret', { expiresIn: '1h' })
+      }, 'salt_secret', { expiresIn: '1h' })
     });
 
   });
